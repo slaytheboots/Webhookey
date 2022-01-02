a8cf02dc950b672da81e21ebb376d8b858cf25d5
mac-andcheese <79228174+mac-andcheese@users.noreply.github.com>
2022-01-02 15:42:03 +0100

    Big update!
    I added an option of making a URL default so that you don't need to type it out/paste every time. (although it won't save because idk how to do that)

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,6 +5,12 @@ using Windows.ApplicationModel.Core;
 using Windows.UI.ViewManagement;
 using Windows.UI;
 using System.Net; //To upload the webhook to Discord
+using Microsoft.Toolkit.Uwp;
+using Microsoft.Toolkit.Uwp.Helpers;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Navigation;
+using System;
+using Windows.Storage;
 
 //Szablon elementu Pusta strona jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x415
 
@@ -16,10 +22,14 @@ namespace Informator
     public sealed partial class MainPage : Page
     {
         private const string V = "Hello Windows";
+        ApplicationDataContainer roamingSettings = null;
+
+        const string SettingName = "exampleSetting";
 
         public MainPage()
         {
             this.InitializeComponent();
+            roamingSettings = ApplicationData.Current.RoamingSettings;
             var applicationView = ApplicationView.GetForCurrentView();
             var titleBar = applicationView.TitleBar;
 
@@ -28,6 +38,27 @@ namespace Informator
             titleBar.ButtonBackgroundColor = Colors.Transparent;
             titleBar.ButtonForegroundColor = Colors.White;
             this.customurl.Text = string.Empty;
+            this.customurl.Visibility = Visibility.Collapsed;
+            this.Default.IsChecked = true;
+            if (SystemInformation.Instance.IsFirstRun)
+            {
+                WelcomeMessage();
+            }
+            else
+            {
+            }
+        }
+
+
+        public static void WelcomeMessage() {
+            ContentDialog dialog = new ContentDialog()
+            {
+                Title = "Notice",
+                Content = "Remember to set your default webhook URL in Settings.",
+                PrimaryButtonText = "Ok"
+            };
+            // Finally, show the dialog
+            dialog.ShowAsync();
         }
 
         public static void sendDiscordWebhook(string URL, string escapedjson)
@@ -40,11 +71,34 @@ namespace Informator
             wr.GetResponse();
         }
 
+        private void defaultradio(object sender, RoutedEventArgs e) {
+        }
+
+        private void customradio(object sender, RoutedEventArgs e)
+        {
+            this.customurl.Visibility = Visibility.Visible;
+            this.theselectors.Visibility = Visibility.Collapsed;
+        }
+
         private void SendClick(object sender, RoutedEventArgs e)
         {
-            string selectedurl = this.customurl.Text;
+            ApplicationDataContainer roamingSettings = Windows.Storage.ApplicationData.Current.RoamingSettings;
+            Windows.Storage.ApplicationDataCompositeValue composite = (ApplicationDataCompositeValue)roamingSettings.Values["RoamingFontInfo"];
+            if (composite != null)
+            {
+                this.defaulturl.Text = composite["defaulturlsetting"] as string;
+            }
+            string selectedurl = this.customurl.Text + this.defaulturl.Text;
             sendDiscordWebhook(selectedurl,
           "{\"username\": \"" + this.type.Text + "\", \"embeds\":[    {\"description\":\"" + this.message.Text + "\", \"title\":\"" + this.title.Text + "\"}]  }");
+        }
+
+        private void girlboss(object sender, RoutedEventArgs e)
+        {
+            roamingSettings.Values[SettingName] = this.defaulturl.Text;
+            Object value = roamingSettings.Values[SettingName];
+            string ayo = Convert.ToString(value);
+            this.defaulturl.Text = Convert.ToString(value);
         }
 
     }
